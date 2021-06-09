4b3e9905599f2d2cbaf004c624169c44f3a3069c
lamccart15 <lamccart15@gmail.com>
2021-06-08 21:58:19 -0400

    Updating part 1 code - adding in button

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -21,5 +21,44 @@ data.forEach(sighting => {
     }) 
 })
 
-// Create filter
-var button = d3.select()
+// Reference button 
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", filterData);
+form.on("submit", filterData);
+
+// Create filter function and check for output
+function filterData () {
+
+    console.log("You clicked the filter")
+
+    // Prevent page from refreshing 
+    d3.event.preventDefault(); 
+
+    // Select input element and raw HTML node
+    var inputElement = d3.select("#datetime"); 
+
+    // Get value of input element
+    var inputValue = inputElement.property("value")
+
+    // Filter data by date
+    var filteredData = data.filter(date => date.datetime === inputValue);
+
+    // Check to ensure data has been filtered
+    console.log(filteredData); 
+
+    // where to put this??? 
+    tbody.html(""); 
+
+    // Return filtered data onto website
+    data.forEach(filteredData => {
+        var row = tbody.append("tr"); 
+        var kvps = Object.entries(filteredData).forEach(([key, value]) => {
+            var td = row.append("td"); 
+            td.text(value)
+        });
+    });
+
+};
